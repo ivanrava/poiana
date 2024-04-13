5d07bc5c478caaf01ad51c53aec1a6c4e1322b16
Ivan Ravasi <ivan4.ravasi@gmail.com>
2024-04-13 14:40:41 +0200

    Better main menu layout

--- scenes/ui/MainMenu.cs
+++ scenes/ui/MainMenu.cs
@@ -7,7 +7,7 @@ public partial class MainMenu : TextureRect
 {
     public override void _Ready()
     {
-        GetNode<Button>("Play").ButtonUp += () =>
+        GetNode<Button>("%Play").ButtonUp += () =>
         {
             var gameScene = GD.Load<PackedScene>("res://scenes/screens/Table.tscn");
             GetNode<GameGlobals>("/root/GameGlobals").ModelMeta = GetNode<Selector>("ModelSelector").SelectedModel();
