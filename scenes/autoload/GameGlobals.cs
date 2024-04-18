f0fd5f7a9ee1d67780ac4044404f9a2a6118fcb2
Ivan Ravasi <ivan4.ravasi@gmail.com>
2024-04-18 22:19:27 +0200

    Adds a fullscreen global toggle

--- scenes/autoload/GameGlobals.cs
+++ scenes/autoload/GameGlobals.cs
@@ -10,4 +10,26 @@ public partial class GameGlobals : Node
     public ModelMeta ModelMeta;
     public bool IsAudioEnabled = true;
     public bool IsFatalityEnabled = true;
+    private bool _fullscreen = true;
+
+    private void SetWindowMode()
+    {
+        DisplayServer.WindowSetMode(_fullscreen
+            ? DisplayServer.WindowMode.Fullscreen
+            : DisplayServer.WindowMode.Windowed);
+    }
+
+    public override void _Ready()
+    {
+        SetWindowMode();
+    }
+
+    public override void _UnhandledKeyInput(InputEvent @event)
+    {
+        if (!@event.IsAction("toggle_fullscreen") || !@event.IsPressed()) return;
+        
+        _fullscreen = !_fullscreen;
+        SetWindowMode();
+        GetViewport().SetInputAsHandled();
+    }
 }
