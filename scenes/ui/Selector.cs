fafbbcddc526adb266f127fd98789c4a9bb2fa0b
Ivan Ravasi <ivan4.ravasi@gmail.com>
2024-05-11 23:15:19 +0200

    Adds italian translation

--- scenes/ui/Selector.cs
+++ scenes/ui/Selector.cs
@@ -10,32 +10,7 @@ public partial class Selector : PanelContainer
     private Label _name;
     private Label _description;
     private int _idxOfSelected;
-    private readonly List<ModelMeta> _modelMetas = new()
-    {
-        new ModelMeta("🤳🏻", "selfplay-best", "DQN self-play", 0.568933f),
-        new ModelMeta("🌊", "amber-lake", "Long training with a sparse reward.", 0.578433f),
-        new ModelMeta("🍂", "autumn-night", "Very long training against the greedy agent.", 0.680767f),
-        new ModelMeta("🦦", "mild-aaldvark", "Only the sparse reward. Greedy agent and quick training.", 0.653067f),
-        new ModelMeta("💫", "lively-cosmos", "Only the sparse reward, with a 256:256 ReLU network.", 0.633833f),
-        new ModelMeta("🍃", "bumbling-leaf", "Only the sparse reward, with a 256:256 ReLU network.", 0.615367f),
-        new ModelMeta("🪨", "easy-shape", "Only sparse reward and a 256:256 ReLU network. Slow annealing.", 0.612867f),
-        new ModelMeta("🌲", "toasty-pine", "Long training with harsher penalties. Pretty hard, less errors.", 0.707933f),
-        new ModelMeta("🐦", "blooming-bird", "Best model with memory", 0.682033f),
-        new ModelMeta("📄", "devout-paper", "Harsher penalties and quick training.", 0.6811f),
-        new ModelMeta("🧨", "cosmic-firebrand", "Worst of the best. Quick training with fast stiffness and lr = 1e-4.", 0.450967f),
-        new ModelMeta("🥗", "dark-salad", "Long training with a bigger lr (3e-3)", 0.572400f),
-        new ModelMeta("🌃", "earnest-night", "Long training with 0.6 exploration annealing. First good model.", 0.588133f),
-        new ModelMeta("🕳️", "graceful-darkness", "Best model with penalties. Trained with all rewards activated.", 0.681433f),
-        new ModelMeta("☄️", "hardy-galaxy", "Smaller learning rate (1e-4) and gamma (0.9)", 0.482300f),
-        new ModelMeta("🎣", "laced-pond", "All rewards against the rules based agent.", 0.676333f),
-        new ModelMeta("🏔️", "rich-mountain", "Further training upon earnest-night, with fast exploration decay.", 0.588133f),
-        new ModelMeta("😌", "skilled-serenity", "Short training with little exploration.", 0.538800f),
-        new ModelMeta("🐲", "smart-dragon", "Long training with dense and sparse rewards. Update / 4 episodes.", 0.532100f),
-        new ModelMeta("🌨️", "snowy-shape", "Standard training, with penalties for suboptimal actions.", 0.650300f),
-        new ModelMeta("❄️", "spring-snowflake", "Further refinement of earnest-night, with a 256:256 ReLU network.", 0.575500f),
-        new ModelMeta("🌟", "true-star", "Second good model obtained. Strong in the second move.", 0.680267f),
-        new ModelMeta("🛶", "warm-river", "Standard refinement upon an already trained model.", 0.576833f),
-    };
+    private readonly List<ModelMeta> _modelMetas = new();
 
     private ProgressBar _winRate;
 
@@ -69,8 +44,9 @@ public partial class Selector : PanelContainer
     }
 }
 
-public record ModelMeta(string Icon, string Name, string Description, float WinRate)
+public record ModelMeta(string Icon, string Name, float WinRate)
 {
+    public string Description;
     public string Filename => Name + ".onnx";
     public string DisplayName => Icon + " " + Name;
 }
--- scenes/ui/SelectorMenu.cs
+++ scenes/ui/SelectorMenu.cs
@@ -1,39 +1,13 @@
-using Godot;
 using System;
+using Godot;
 using System.Collections.Generic;
 using System.Linq;
 using PoIAna.scenes.autoload;
-using PoIAna.scenes.cards;
 using PoIAna.scenes.ui;
 
 public partial class SelectorMenu : TextureRect
 {
-    private readonly List<ModelMeta> _modelMetas = new()
-    {
-        new ModelMeta("🤳🏻", "selfplay-best", "DQN self-play", 0.568933f),
-        new ModelMeta("🌊", "amber-lake", "Long training with a sparse reward.", 0.578433f),
-        new ModelMeta("🍂", "autumn-night", "Very long training against the greedy agent.", 0.680767f),
-        new ModelMeta("🦦", "mild-aardvark", "Only the sparse reward. Greedy agent and quick training.", 0.653067f),
-        new ModelMeta("💫", "lively-cosmos", "Only the sparse reward, with a 256:256 ReLU network.", 0.633833f),
-        new ModelMeta("🍃", "bumbling-leaf", "Only the sparse reward, with a 256:256 ReLU network.", 0.615367f),
-        new ModelMeta("🪨", "easy-shape", "Only sparse reward and a 256:256 ReLU network. Slow annealing.", 0.612867f),
-        new ModelMeta("🌲", "toasty-pine", "Long training with harsher penalties. Pretty hard, less errors.", 0.680800f),
-        new ModelMeta("🐦", "blooming-bird", "One of the first models with penalties.", 0.682033f),
-        new ModelMeta("📄", "devout-paper", "Harsher penalties and quick training.", 0.656833f),
-        new ModelMeta("🧨", "cosmic-firebrand", "Worst of the best. Quick training with fast stiffness and lr = 1e-4.", 0.450967f),
-        new ModelMeta("🥗", "dark-salad", "Long training with a bigger lr (3e-3)", 0.572400f),
-        new ModelMeta("🌃", "earnest-night", "Long training with 0.6 exploration annealing. First good model.", 0.588133f),
-        new ModelMeta("🕳️", "graceful-darkness", "Best model with penalties. Trained with all rewards activated.", 0.681433f),
-        new ModelMeta("☄️", "hardy-galaxy", "Smaller learning rate (1e-4) and gamma (0.9)", 0.482300f),
-        new ModelMeta("🎣", "laced-pond", "All rewards against the rules based agent.", 0.676333f),
-        new ModelMeta("🏔️", "rich-mountain", "Further training upon earnest-night, with fast exploration decay.", 0.588133f),
-        new ModelMeta("😌", "skilled-serenity", "Short training with little exploration.", 0.538800f),
-        new ModelMeta("🐲", "smart-dragon", "Long training with dense and sparse rewards. Update / 4 episodes.", 0.532100f),
-        new ModelMeta("🌨️", "snowy-shape", "Standard training, with penalties for suboptimal actions.", 0.650300f),
-        new ModelMeta("❄️", "spring-snowflake", "Further refinement of earnest-night, with a 256:256 ReLU network.", 0.575500f),
-        new ModelMeta("🌟", "true-star", "Second good model obtained. Strong in the second move.", 0.680267f),
-        new ModelMeta("🛶", "warm-river", "Standard refinement upon an already trained model.", 0.576833f),
-    };
+    private List<ModelMeta> _modelMetas;
     private ModelMeta _selectedModel;
     
     private void ModulateStyleBox(Button button, string styleBoxName, Color color)
@@ -56,6 +30,38 @@ public partial class SelectorMenu : TextureRect
     public override void _Ready()
     {
         base._Ready();
+        
+        _modelMetas = new()
+        {
+            new ModelMeta("🤳🏻", "selfplay-best", 0.568933f),
+            new ModelMeta("🌊", "amber-lake", 0.578433f),
+            new ModelMeta("🍂", "autumn-night", 0.680767f),
+            new ModelMeta("🦦", "mild-aardvark", 0.653067f),
+            new ModelMeta("💫", "lively-cosmos", 0.633833f),
+            new ModelMeta("🍃", "bumbling-leaf", 0.615367f),
+            new ModelMeta("🪨", "easy-shape", 0.612867f),
+            new ModelMeta("🌲", "toasty-pine", 0.680800f),
+            new ModelMeta("🐦", "blooming-bird", 0.682033f),
+            new ModelMeta("📄", "devout-paper", 0.656833f),
+            new ModelMeta("🧨", "cosmic-firebrand", 0.450967f),
+            new ModelMeta("🥗", "dark-salad", 0.572400f),
+            new ModelMeta("🌃", "earnest-night", 0.588133f),
+            new ModelMeta("🕳️", "graceful-darkness", 0.681433f),
+            new ModelMeta("☄️", "hardy-galaxy", 0.482300f),
+            new ModelMeta("🎣", "laced-pond", 0.676333f),
+            new ModelMeta("🏔️", "rich-mountain", 0.588133f),
+            new ModelMeta("😌", "skilled-serenity", 0.538800f),
+            new ModelMeta("🐲", "smart-dragon", 0.532100f),
+            new ModelMeta("🌨️", "snowy-shape", 0.650300f),
+            new ModelMeta("❄️", "spring-snowflake", 0.575500f),
+            new ModelMeta("🌟", "true-star", 0.680267f),
+            new ModelMeta("🛶", "warm-river", 0.576833f),
+        };
+        foreach (var meta in _modelMetas)
+        {
+            meta.Description = Tr(meta.Name.ToUpper());
+        }
+        
         _modelMetas.Sort((meta1, meta2) => meta2.WinRate.CompareTo(meta1.WinRate));
         var container = GetNode<HFlowContainer>("%HFlowContainer");
         GetNode<Button>("%Play").ButtonUp += () =>
@@ -82,7 +88,7 @@ public partial class SelectorMenu : TextureRect
             {
                 GetNode<Label>("%DisplayName").Text = modelMeta.DisplayName;
                 GetNode<Label>("%Description").Text = modelMeta.Description;
-                GetNode<RichTextLabel>("%WinRate").Text = $"[center]This model has an averaged [b]{modelMeta.WinRate * 100}%[/b] win rate against easy to hard heuristic agents.[/center]";
+                GetNode<RichTextLabel>("%WinRate").Text = String.Format(Tr("WINRATE"), modelMeta.WinRate * 100);
                 GetNode<Button>("%Play").Show();
                 _selectedModel = modelMeta;
             };
